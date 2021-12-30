ec9abf9d7a43b821f882152301da0f2406aba26a
Teyanna Earle <teyonna.earle92@gmail.com>
2021-12-30 02:59:59 -0500

    Begin topNav styling

--- src/components/nav/TopNav.js
+++ src/components/nav/TopNav.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Logo from "../../assets/Logo.png"
 import { Link } from "react-router-dom" 
+import Logo from "../../assets/Logo.png"
 import "./TopNav.scss"
 
 function TopNav() {
     return (
         <div className="topNav">
             <Link to="/" className="topNav__logo"> <img src={Logo}/> </Link>
+            <div className="topNav__menuItems">
+               <ul>
+                <li><Link to="/about"> who am i ? </Link></li>
+                <li><Link to="/projects" className="topNav__menuItems-middle"> what have i made ? </Link></li>
+                <li><Link to="/contact"> how can we connect ? </Link></li>
+               </ul>
+            </div>
         </div>
     )
 }
