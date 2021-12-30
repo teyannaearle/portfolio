31064cf4e37020121e8019e879c5b2a47d455e70
Teyanna Earle <teyonna.earle92@gmail.com>
2021-12-29 23:10:41 -0500

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.scss';
+import { Routes, Route } from "react-router-dom"
+import TopNav from "./components/nav/TopNav"
+import SideNav from "./components/nav/SideNav"
+import Footer from "./components/Footer"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
+      <SideNav />
+      <Footer />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
 
     </div>
   );
---
+++ src/components/nav/TopNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopNav() {
+    return (
+        <div>
+            <h1> Nav </h1>
+        </div>
+    )
+}
+
+export default TopNav
