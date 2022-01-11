c0a414d37648de8ede19eab4492a2cb1241b50bf
Teyanna Earle <teyonna.earle92@gmail.com>
2022-01-11 15:22:04 -0500

    adjust animation timing on home pg

--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -4,12 +4,12 @@ import "./HomeContent.scss"
 function HomeContent() {
     return (
         <div className="homeContent">
-            <p className="homeContent__name">Té - yanna Earle .</p>
-            <p className="homeContent__pronounce">( tay - ahh - nuh  <>&nbsp;</> er-l )</p>
-            <p className="homeContent__heading">Full Stack Web Dev</p>
-            <p className="homeContent__explanations"> I make web applications, and have fun doing it. </p>
-            <p className="homeContent__heading">Wellness Fanatic</p>
-            <p className="homeContent__explanations"> Always working to better my life and the lives of those around me. </p>
+            <p className="homeContent__name homeContent__slideIn-first">Té - yanna Earle </p>
+            <p className="homeContent__pronounce homeContent__slideIn-first">( tay - ahh - nuh  <>&nbsp;</> er-l )</p>
+            <p className="homeContent__heading homeContent__slideIn-second">Full Stack Web Dev</p>
+            <p className="homeContent__explanations homeContent__slideIn-second"> I make web applications, and have fun doing it. </p>
+            <p className="homeContent__heading homeContent__slideIn-third">Wellness Fanatic</p>
+            <p className="homeContent__explanations homeContent__slideIn-third"> Always working to better my life and the lives of those around me. </p>
         </div>
     )
 }
