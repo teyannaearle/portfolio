bfdd0a46f7c2d4e5d21a3415cef345b3ec569af8
Teyanna Earle <teyonna.earle92@gmail.com>
2022-01-11 22:59:04 -0500

    style connect pg

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,32 +1,58 @@
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser';
-import "./ContactForm.scss"
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./ContactForm.scss";
 
 function ContactForm() {
-    const form = useRef();
-    const userId = process.env.REACT_APP_EMAILJS_USER_ID
-    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
-    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
+  const form = useRef();
+  const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
 
-    const sendEmail = (e) => {
-        e.preventDefault()
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm(`${serviceId}`, `${templateId}` , form.current, `${userId}`)
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    }
+    emailjs
+      .sendForm(`${serviceId}`, `${templateId}`, form.current, `${userId}`)
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-    return (
-        <form ref={form} className="contactForm" onSubmit={sendEmail}>
-           <input className="contactForm__input" type="text" name="fullName" placeholder ="Enter Full Name" required />
-           <input className="contactForm__input" type="text" name="email" placeholder ="Enter Email" required />
-           <textarea name="message" required />
-           <button type="submit">Submit </button>
-        </form>
-    )
+  return (
+    <div className="contactFormSection">
+        <div className="contactFormSection__card">
+        <p>Feel free to contact me (and/or tell me a little about yourself) if you'd like to work together !</p>
+
+      
+      <form ref={form} className="contactFormSection__form" onSubmit={sendEmail}>
+        <label for="fullName"> Your Name :</label>
+        <input
+          className="contactForm__input"
+          type="text"
+          name="fullName"
+          placeholder="(Required)"
+          required
+        />
+        <label for="email">Your Email : </label>
+        <input
+          className="contactForm__input"
+          type="text"
+          name="email"
+          placeholder="(Required)"
+          required
+        />
+        <label for="message">Your Message : </label>
+        <textarea name="message" placeholder="Your Message ... " required />
+        <button type="submit">Let's Connect !</button>
+      </form>
+      </div>
+  </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import "./Contact.scss"
-import Resume from "../components/Resume"
-import ContactForm from "../components/ContactForm"
-import caterpillar from "../assets/caterpillar.png"
+import React from "react";
+import "./Contact.scss";
+import Resume from "../components/Resume";
+import ContactForm from "../components/ContactForm";
+import caterpillar from "../assets/caterpillar.png";
 
 function Contact() {
-    return (
-        <div className="contactPage">
-            <Resume />
-            <img src={caterpillar} alt="alice in wonderland caterpillar saying who are you"/>
-            <ContactForm />
-        </div>
-    )
+  return (
+    <div className="contactPage">
+      <Resume />
+      <div className="contactPage__greeting">
+        {" "}
+        <h1>I'm so happy that you're here.</h1>
+      </div>
+
+      <div className="contactPage__bottomHalf">
+        <img
+          src={caterpillar}
+          alt="alice in wonderland caterpillar saying who are you"
+        />
+        <ContactForm />
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
