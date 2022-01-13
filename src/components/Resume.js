f5b805b3fe4268d4972bd56c886c8e5350b7eb66
Teyanna Earle <teyonna.earle92@gmail.com>
2022-01-12 22:19:15 -0500

    fix margins across site

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { HiExternalLink } from "react-icons/hi"
-import "./Resume.scss"
+import React from "react";
+import { HiExternalLink } from "react-icons/hi";
+import "./Resume.scss";
 
 function Resume() {
-    return (
-        <div className="resumeLink">< a href = "https://docs.google.com/document/d/e/2PACX-1vTfIJ6wYZfBlTa4qz0y20zKXx0Hpma9aPw00OM7NWHBrWKdyBTXcbRZnwoQBe9iTX2_gEP2MuXzPnnH/pub"  target="_blank" rel="noreferrer"> View Resume <HiExternalLink /> </a></div>
-
-    )
+  return (
+    <div className="resumeLink">
+      <a
+        href="https://docs.google.com/document/d/e/2PACX-1vTfIJ6wYZfBlTa4qz0y20zKXx0Hpma9aPw00OM7NWHBrWKdyBTXcbRZnwoQBe9iTX2_gEP2MuXzPnnH/pub"
+        target="_blank"
+        rel="noreferrer"
+      >
+        View Resume <HiExternalLink />
+      </a>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,12 +2,35 @@ import React from "react";
 import Project from "../components/Project";
 import Resume from "../components/Resume";
 import projectsData from "../data/projectsData";
+import {
+  FaGit,
+  FaGitAlt,
+  FaNodeJs,
+  FaReact,
+  FaHtml5,
+  FaCss3Alt,
+} from "react-icons/fa";
+import { SiPostgresql, SiRedux, SiJavascript } from "react-icons/si";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import "./Projects.scss";
 
 function Projects() {
   return (
     <div className="projectPage">
       <Resume />
+      <ul className="projectPage__skills">
+        <FaGitAlt />
+        <FaNodeJs />
+        <SiPostgresql />
+        <SiRedux />
+        <p>
+          <BsArrowLeft /> Skills <BsArrowRight />
+        </p>
+        <FaReact />
+        <SiJavascript />
+        <FaHtml5 />
+        <FaCss3Alt />
+      </ul>
       <ul className="projectPage__container">
         {projectsData.map((project, i) => {
           return <Project project={project} key={i} />;
