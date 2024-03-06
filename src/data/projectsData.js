e216b4701ad1d1d23f543ebfd4e9b5cc5b91bb3f
Teyanna Earle <teyonna.earle92@gmail.com>
2024-03-06 16:29:59 -0500

    fix sentence in project description

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -17,7 +17,7 @@ const projectsData = [
   }, 
   {
     src: calcGif,
-    caption:"A basic React calculator. Add, subtract, multiply and divide to your heart's content",
+    caption:"A basic React calculator. Add, subtract, multiply and divide to your heart's content.",
     technologies: ["React", "CSS", "Netlify"],
     title:"React Calculator",
     githubLink:"https://github.com/teyannaearle/react_calculator",
