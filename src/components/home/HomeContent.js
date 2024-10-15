9f03702c570897cfd24315c2b4f64a7441b201c1
Teyanna Earle <75263239+teyannaearle@users.noreply.github.com>
2024-10-14 23:14:35 -0400

    Remove hyphen from first name in HomeContent.js

--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -4,7 +4,7 @@ import "./HomeContent.scss"
 function HomeContent() {
     return (
         <div className="homeContent">
-            <p className="homeContent__name homeContent__slideIn-first">Té - yanna Earle </p>
+            <p className="homeContent__name homeContent__slideIn-first">Téyanna Earle </p>
             <p className="homeContent__pronounce homeContent__slideIn-first">( tay-ahh-nuh er-l )</p>
             <p className="homeContent__heading homeContent__slideIn-second">Full Stack Web Dev</p>
             <p className="homeContent__explanations homeContent__slideIn-second"> I make web applications, and have fun doing it. </p>
