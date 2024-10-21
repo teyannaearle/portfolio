7b9a1d0c64969f3a1248752aa6dcbbe56ef1a518
Teyanna Earle <teyonna.earle92@gmail.com>
2024-10-20 23:46:58 -0400

    update link to projects

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
           <Routes>
             <Route path="/" element={<About />} />
             {/* <Route path="/about" element={<About />} /> */}
-            {/* <Route path="/projects" element={<Projects />} /> */}
+            <Route path="/projects" element={<Projects />} />
             {/* <Route path="/contact" element={<Contact />} /> */}
           </Routes>
         </div>
--- src/components/project/ProjectsLink.js
+++ src/components/project/ProjectsLink.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaLaptopCode } from "react-icons/fa";
 import "./Project.scss";
 
 function ProjectsLink() {
   return (
     <div className="projectsLink">
-      <a href="/projects">
+      <Link to="/projects">
+        {" "}
         View Projects <FaLaptopCode />
-      </a>
+      </Link>
+      {/* <a href="/projects">
+        View Projects <FaLaptopCode />
+      </a> */}
     </div>
   );
 }
